af1069fc082d37466c27d2aee4918e49c612bef0
Nizam Uddin Babu <43912849+nizamseu@users.noreply.github.com>
2026-03-10 12:57:06 +0600

    chore: update dependencies for 3D rendering support
    - Added @react-three/drei and @react-three/fiber for enhanced 3D capabilities
    - Included three.js for 3D graphics rendering

--- app/animation/page.js
+++ app/animation/page.js
@@ -1,14 +1,29 @@
-"use client"
-import { ReactLenis, useLenis } from 'lenis/react'
-export default function App() {
-  const lenis = useLenis((lenis) => {
-    // called every scroll
-    console.log(lenis)
-  })
+"use client";
 
-	return (
-		<div className="flex justify-center items-center h-screen">
-			<div className="box text-9xl font-bold">Animation</div>
-		</div>
-	);
+import { Billboard, DragControls, Text } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+export default function animation() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Canvas>
+        <Billboard
+          follow={true}
+          lockX={false}
+          lockY={false}
+          lockZ={false} // Lock the rotation on the z axis (default=false)
+        >
+          <Text fontSize={1}>I'm a billboard</Text>
+        </Billboard>
+
+        {/* <DragControls>
+          <mesh position={[0, 0, 1]} rotation={[0.5, 0.5, 1]}>
+            <sphereGeometry attach="geometry" />
+            <meshStandardMaterial attach="material" />
+          </mesh>
+        </DragControls> */}
+        <directionalLight position={[1, 1, 3]} color="yellow" />
+      </Canvas>
+    </div>
+  );
 }
